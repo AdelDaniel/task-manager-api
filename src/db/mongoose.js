1f7d8e8295e8122092fe93f90b5a3085f854f443
adel_daniel <adel.daniel.222@gmail.com>
2021-09-27 19:22:48 +0200

    update the mongodb url

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const connectionURL = process.env.MONGODB_URL; // connect local host server
-const databaseName = process.env.MONGODB_NAME; // the database name you want
+const connectionURL = process.env.MONGODB_URL; // connect local host server and the database name you want
 
-mongoose.connect(`${connectionURL}/${databaseName}`, {
+mongoose.connect(`${connectionURL}`, {
   useNewUrlParser: true,
 });
